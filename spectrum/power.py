984cd561a4cd9625413813f0a8638a7afbd31452
Tom Winch <tomw@flax.co.uk>
2016-06-28 10:23:44 +0100

    Enable to be run without GPIO

--- spectrum/power.py
+++ spectrum/power.py
@@ -1,15 +1,23 @@
 from config import *
 from time import sleep
-import RPi.GPIO as GPIO
 
-def power_on():
-  GPIO.setmode(GPIO.BCM) #There are two ways of numbering pins. We choose I/O chip numbering
+try:
+  # if we're on a Pi, we can import GPIO and use it to turn on the rig
+  import RPi.GPIO as GPIO
 
-  GPIO.setup(RADIO_ON_SWITCH, GPIO.OUT, initial=GPIO.LOW)
-  sleep(RADIO_ON_SLEEP_SECS)
-  GPIO.output(RADIO_ON_SWITCH, GPIO.HIGH)
-  sleep(RADIO_ON_SLEEP_SECS)
-  GPIO.output(RADIO_ON_SWITCH, GPIO.LOW)
-  sleep(RADIO_ON_SLEEP_SECS)
+  def power_on():
+    GPIO.setmode(GPIO.BCM) #There are two ways of numbering pins. We choose I/O chip numbering
 
-  GPIO.cleanup()
+    GPIO.setup(RADIO_ON_SWITCH, GPIO.OUT, initial=GPIO.LOW)
+    sleep(RADIO_ON_SLEEP_SECS)
+    GPIO.output(RADIO_ON_SWITCH, GPIO.HIGH)
+    sleep(RADIO_ON_SLEEP_SECS)
+    GPIO.output(RADIO_ON_SWITCH, GPIO.LOW)
+    sleep(RADIO_ON_SLEEP_SECS)
+
+    GPIO.cleanup()
+
+except ImportError:
+  # otherwise, nothing we can do
+  def power_on():
+    pass
