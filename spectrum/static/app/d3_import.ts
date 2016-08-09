26062a4d11eede262180486871c3f1ac6ebf0201
Tom Winch <tomw@flax.co.uk>
2016-08-09 15:03:06 +0100

    Fix waterfall chart timestamp tick labels

--- spectrum/static/app/d3_import.ts
+++ spectrum/static/app/d3_import.ts
@@ -6,13 +6,14 @@ export var dt_format = d3.time.format("%d/%m/%Y %X");
 
 export var insertLineBreaks = function (d) {
   var el = d3.select(this);
+  var x = el.attr('x') || 0;
   var words = dt_format(d).split(' ');
   el.text('');
 
   for (let i = 0; i < words.length; i++) {
     var tspan = el.append('tspan').text(words[i]);
     if (i > 0) {
-      tspan.attr('x', 0).attr('dy', '15');
+      tspan.attr('x', x).attr('dy', '15');
     }
   }
 }
