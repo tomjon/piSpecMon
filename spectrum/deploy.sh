4813ea8224ede12724105c14f32c71720135660d
Tom Winch <tom.j.winch@gmail.com>
2016-08-11 09:56:04 +0000

    Only install nodejs is not already there

--- spectrum/deploy.sh
+++ spectrum/deploy.sh
@@ -1,4 +1,8 @@
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install nodejs
-cd static
-npm run tsc
+#!/bin/bash
+hash npm 2>/dev/null || {
+  echo "Bang"
+  exit 1;
+  curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+  sudo apt-get install nodejs
+}
+cd static; npm run tsc
