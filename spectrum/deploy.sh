a7dc38943bd870af558fb9026a88aa18044da669
tomjon <tom.j.winch@gmail.com>
2016-09-21 14:33:22 +0000

    Create version file

--- spectrum/deploy.sh
+++ spectrum/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# create the version file
+echo `git describe --tags` | sudo tee /version >/dev/null
+
+# compile typescript into javascript
 hash npm 2>/dev/null || {
   curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
   sudo apt-get install nodejs
