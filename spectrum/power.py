0c28d2c1e67fd9fa8b49448cfa43b5c8c40e8513
tomjon <tom.j.winch@gmail.com>
2016-10-12 11:44:37 +0000

    Open the rig before attempting to turn off

--- spectrum/power.py
+++ spectrum/power.py
@@ -31,7 +31,10 @@ def power_off():
     rig = data_store.Settings('rig').read()
     parse_config(rig.values)
     monitor = Monitor(**rig.values)
+    monitor.open()
     monitor.power_off()
+    monitor.close()
+
 
 
 if __name__ == "__main__":
