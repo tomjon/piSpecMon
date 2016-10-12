0b77cf282702b9f3060ab247f3867cde96d49af3
Tom Winch <tomw@flax.co.uk>
2016-10-12 12:01:01 +0100

    Improve code quality

--- spectrum/monkey.py
+++ spectrum/monkey.py
@@ -1,11 +1,9 @@
 """ Module defining the Monkey process, for decoding RDS using the Monkey board.
 """
+from time import sleep, time
 from config import MONKEY_PID, MONKEY_CONFIG, MONKEY_STATUS, MONKEY_POLL
 from common import log, parse_config, scan, now, StoreError
 from process import Process, UpdatableDict
-from time import sleep, time
-import requests
-import fs_datastore as data_store
 try:
     from rds import RdsApi
 except ImportError:
@@ -32,6 +30,8 @@ def _iterator(config):
     with RdsApi(rds['device']) as api:
         while True:
             for idx, freq in scan(**scan_config):
+                log.debug("Scanning %s", freq)
+
                 status = UpdatableDict()
                 yield status('freq_n', idx)
 
@@ -49,8 +49,9 @@ def _iterator(config):
                 if not ok: # pylint: disable=undefined-loop-variable
                     continue
 
+                log.debug("Found RDS name %s", name) # pylint: disable=undefined-loop-variable
                 try:
-                    config.write_rds_name(now(), idx, name)
+                    config.write_rds_name(now(), idx, name) # pylint: disable=undefined-loop-variable
                 except StoreError:
                     return
 
@@ -62,6 +63,7 @@ def _iterator(config):
 
                     if text is not None and text != text_0:
                         text_0 = text
+                        log.debug("Found RDS text %s", text)
                         try:
                             config.write_rds_text(now(), idx, text)
                         except StoreError:
@@ -78,6 +80,7 @@ class Monkey(Process):
 
 
 if __name__ == "__main__":
+    #pylint: disable=invalid-name
     monkey = Monkey()
     monkey.init()
-    monkey.start(iterator)
+    monkey.start(_iterator)
