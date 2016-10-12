975059fe596a9361b499ead070534176fe0262e9
Tom Winch <tomw@flax.co.uk>
2016-10-12 18:03:26 +0100

    Improve code quality

--- spectrum/rds.py
+++ spectrum/rds.py
@@ -1,6 +1,5 @@
 """ Module defines a wrapper for the Keystone DLL.
 """
-
 from ctypes import cdll, create_unicode_buffer, CDLL, c_char_p, c_long, c_int, pointer
 from ctypes.util import find_library
 from os.path import exists
@@ -9,49 +8,50 @@ from os.path import exists
 LIBRARY = 'keystonecomm'
 MODE_FM = 1
 
-_buffer = create_unicode_buffer(300)
 
-_library = find_library(LIBRARY)
-if _library is not None:
-    cdll.LoadLibrary(_library)
-    _dll = CDLL(_library)
-else:
-    raise ImportError("Could not find library {0}".format(LIBRARY))
+# load the DLL
+def _load_dll():
+    library = find_library(LIBRARY)
+    if library is None:
+        raise ImportError("Could not find library {0}".format(LIBRARY))
+    cdll.LoadLibrary(library)
+    return CDLL(library)
 
+DLL = _load_dll()
 
 class RdsApi(object):
     """ Ctypes wrapper for the Keystone monkey board comms DLL.
     """
-
     def __init__(self, device, use_hard_mute=True):
         if not exists(device):
             raise Exception("No device {0}".format(device))
         self.device = device
         self.use_hard_mute = use_hard_mute
+        self._buffer = create_unicode_buffer(300)
 
     def __enter__(self):
-        value = _dll.OpenRadioPort(c_char_p(self.device), self.use_hard_mute)
+        value = DLL.OpenRadioPort(c_char_p(self.device), self.use_hard_mute)
         if value != 1:
             raise Exception("Error opening monkey board: {0}".format(value))
         return self
 
     def __exit__(self, *args):
-        value = _dll.CloseRadioPort()
+        value = DLL.CloseRadioPort()
         if value != 1:
             raise Exception("Error closing monkey board: {0}".format(value))
 
-    def set_frequency(self, freq):
+    def set_frequency(self, freq): # pylint: disable=no-self-use
         """ Set RDS decode frequency.
         """
-        value = _dll.PlayStream(c_char_p(MODE_FM), c_long(int(freq / 1000)))
+        value = DLL.PlayStream(c_char_p(MODE_FM), c_long(int(freq / 1000)))
         if value != 1:
             raise Exception("Error setting frequencya: {0}".format(value))
 
-    def get_strength(self):
+    def get_strength(self): # pylint: disable=no-self-use
         """ Read the signal strength.
         """
         error = pointer(c_int(0))
-        strength = _dll.GetSignalStrength(error)
+        strength = DLL.GetSignalStrength(error)
         if error.contents.value != 0:
             raise Exception("Error reading signal strength: {0}".format(error.contents.value))
         return strength
@@ -59,11 +59,11 @@ class RdsApi(object):
     def get_name(self):
         """ Read the decoded RDS name string.
         """
-        value = _dll.GetProgramName(c_char_p(MODE_FM), c_long(0), c_char_p(1), _buffer)
-        return _buffer.value.strip() if value > 0 else None
+        value = DLL.GetProgramName(c_char_p(MODE_FM), c_long(0), c_char_p(1), self._buffer)
+        return self._buffer.value.strip() if value > 0 else None
 
     def get_text(self):
         """ Read the decoded RDS text string.
         """
-        value = _dll.GetProgramText(_buffer)
-        return _buffer.value.strip() if value == 0 else None
+        value = DLL.GetProgramText(self._buffer)
+        return self._buffer.value.strip() if value == 0 else None
