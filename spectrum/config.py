3c26626508d11cb7b8431ffb738578f801242e62
Tom Winch <tomw@flax.co.uk>
2016-10-17 12:54:42 +0100

    Use YAML for config file format

--- spectrum/config.py
+++ spectrum/config.py
@@ -1,63 +1,39 @@
-""" Configuration constants.
+""" Parse config in YAML format. Starts with the default config file, then applies
+    settings from any .yml paths found on the command line.
 """
+import sys
+import yaml
 import Hamlib
 
-#FIXME use yaml
-LOG_SIZE = 1024 * 1024
+CONFIG_FILE = 'psm.yml'
 
-VERSION_FILE = '/version'
+# load configuration settings from the YML file at the given path, converting keys
+# to upper case and concatenating keys from the top two levels of dictionaries
+# (so you can't have dictionaries for values at the top level)
+def _load_settings(path):
+    def _setattr(key, value):
+        setattr(sys.modules[__name__], key.upper(), value)
 
-# path where Flask's secret key is stored
-SECRET_KEY = 'secret.key'
+    with open(path) as f:
+        config = yaml.load(f)
+        for key, value in config.iteritems():
+            if isinstance(value, dict):
+                for key2, value2 in value.iteritems():
+                    _setattr('_'.join((key, key2)), value2)
+            else:
+                _setattr(key, value)
 
-# worker configuration
-WORKER_PID = '.worker_pid'
-WORKER_CONFIG = '.worker_config'
-WORKER_STATUS = '.worker_status'
+# load settings from any YML files specified on the command line (overriding previous)
+def _parse_args():
+    for arg in sys.argv:
+        if arg.endswith('.yml'):
+            _load_settings(arg)
 
-ELASTICSEARCH = 'http://localhost:9200/'
-EXPORT_DIRECTORY = '/tmp'
+_load_settings(CONFIG_FILE)
+_parse_args()
 
-USERS_FILE = 'users.passwords'
-ROUNDS = 10 ** 5
-
-# Monkey config (RDS decoder)
-MONKEY_PID = '.monkey_pid'
-MONKEY_CONFIG = '.monkey_config'
-MONKEY_STATUS = '.monkey_status'
-MONKEY_POLL = 1.0
-
-# GPIO radio 'on' script settings
-RADIO_ON_SWITCH = 21 # BCM numbering
-RADIO_ON_SLEEP_SECS = 1.0
-
-# user session inactivity timeout
-USER_TIMEOUT_SECS = 60
-
-# data directories
-DATA_PATH = 'data'
-SAMPLES_PATH = 'samples'
-SETTINGS_PATH = 'settings'
-
-# how often (in s) the wav2mp3 converter should run
-CONVERT_PERIOD = 300
-
-# default settings
+# set the default rig model based on whether the PSM test model is available in Hamlib
 try:
-    DEFAULT_RIG_SETTINGS = {'model': Hamlib.RIG_MODEL_PSMTEST}
+    DEFAULT_RIG_SETTINGS['model'] = Hamlib.RIG_MODEL_PSMTEST # pylint: disable=undefined-variable
 except AttributeError:
-    DEFAULT_RIG_SETTINGS = {
-        'model': Hamlib.RIG_MODEL_AR8200, 'pathname': '/foo', 'data_bits': 8,
-        'rate': 9600, 'stop_bits': 1, 'parity': 2, 'attenuation': False,
-        'radio_on': 2, 'set_check': 2, 'retries': 2, 'interval': 1000, 'write_delay': 50
-    }
-DEFAULT_AUDIO_SETTINGS = {
-    'path': '/dev/dsp1', 'rate': 44100, 'period': 600, 'duration': 10, 'threshold': -20
-}
-DEFAULT_RDS_SETTINGS = {
-    'device': '/dev/ttyACM0', 'strength_threshold': 40, 'strength_timeout': 20, 'rds_timeout': 300
-}
-DEFAULT_SCAN_SETTINGS = {
-    'freqs': {'range': [87.5, 108.0, 0.1], 'exp': 6, 'freqs': [{'f': '', 'exp': 6}]},
-    'monitor': {'radio_on': 1}, 'scan': {'mode': 64, 'rds': True, 'audio': False}
-}
+    DEFAULT_RIG_SETTINGS['model'] = Hamlib.RIG_MODEL_AR8200 # pylint: disable=undefined-variable
