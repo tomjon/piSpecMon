332addf1e8abb96903fa5bf084f7167e9d43d9d1
Tom Winch <tomw@flax.co.uk>
2016-10-18 15:02:16 +0100

    Remove unused constant

--- spectrum/static/app/constants.ts
+++ spectrum/static/app/constants.ts
@@ -8,5 +8,3 @@ export var FREQUENCY_CHART_OPTIONS: any = { y_axis: [-70, 70, 10], margin: { top
 export var LEVEL_CHART_OPTIONS: any = { y_axis: [-70, 70, 10], margin: { top: 30, left: 40, right: 200, bottom: 40 }, width: CHART_WIDTH, height: CHART_HEIGHT, x_ticks: 7 };
 export var WATERFALL_CHART_OPTIONS: any = { heat: [-70, 0, 70], margin: { top: 25, left: 80, right: 80, bottom: 30 }, width: CHART_WIDTH, height: CHART_HEIGHT, y_ticks: 7 };
 export var AUDIO_CHART_OPTIONS: any = { margin: { top: 20, left: 80, right: 80, bottom: 30 }, width: CHART_WIDTH, height: CHART_HEIGHT, y_ticks: 10 };
-
-export var CHANGE_TIMEOUT = 5000;
