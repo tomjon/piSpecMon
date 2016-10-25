86e9b54d09d3facd1935f1e8a43bb3408d9a5436
Tom Winch <tomw@flax.co.uk>
2016-10-25 15:10:19 +0100

    Ensure email subject line contains date to make it unqiue

--- spectrum/bin/mail.sh
+++ spectrum/bin/mail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 while ! ping -c1 smtp.gmail.com &>/dev/null; do sleep 2; done
-echo "`/sbin/ifconfig`" | mail -a "From: pispecmon@gmail.com" -s `uname -n` -c pispecmon@gmail.com remote.systems@ofcom.org.uk
+echo "`/sbin/ifconfig`" | mail -a "From: pispecmon@gmail.com" -s `uname -n; date` -c pispecmon@gmail.com remote.systems@ofcom.org.uk
