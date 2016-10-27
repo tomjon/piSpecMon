0c9d25fbcba8afae03414a8125d466be616f66cc
Tom Winch <tomw@flax.co.uk>
2016-10-27 14:21:22 +0100

    Remove test main

--- spectrum/tail.py
+++ spectrum/tail.py
@@ -45,27 +45,3 @@ def iter_tail(f, n, level='\n'):
                 if count < n:
                     yield line[i:j+1]
             i = j + 1
-
-
-if __name__ == "__main__":
-    # pylint: disable=invalid-name
-    import StringIO
-
-    _f = StringIO.StringIO("""2016-10-03 10:01:44,838 - INFO - "GET /monitor HTTP/1.1" 200 -
-2016-10-03 10:02:41,003 - DEBUG - No message
-2016-10-03 10:03:10,123 - DEBUG - Continue
-2016-10-03 10:03:12,412 - INFO - Complete""")
-
-    lines = list(iter_tail(_f, 3))
-    assert len(lines) == 3
-    assert lines[0] == "2016-10-03 10:02:41,003 - DEBUG - No message\n"
-    assert lines[-1] == "2016-10-03 10:03:12,412 - INFO - Complete"
-
-    lines = list(iter_tail(_f, 2, 'INFO'))
-    assert len(lines) == 2
-    assert lines[0] == "2016-10-03 10:01:44,838 - INFO - \"GET /monitor HTTP/1.1\" 200 -\n"
-    assert lines[-1] == "2016-10-03 10:03:12,412 - INFO - Complete"
-
-    lines = list(iter_tail(_f, 1, 'DEBUG'))
-    assert len(lines) == 1
-    assert lines[0] == "2016-10-03 10:03:10,123 - DEBUG - Continue\n"
