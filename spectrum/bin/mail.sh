0e2c412c88698d4c8098984a0cd482f894509b27
tomjon <tom.j.winch@gmail.com>
2016-10-31 14:09:30 +0000

    Fix email command

--- spectrum/bin/mail.sh
+++ spectrum/bin/mail.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 while ! ping -c1 smtp.gmail.com &>/dev/null; do sleep 2; done
-echo "`/sbin/ifconfig`" | mail -a "From: pispecmon@gmail.com" -s `uname -n; date` -c pispecmon@gmail.com remote.systems@ofcom.org.uk
+SUBJECT=`uname -n; date`
+echo "`/sbin/ifconfig`" | mail -a "From: pispecmon@gmail.com" -s "$SUBJECT" -c pispecmon@gmail.com remote.systems@ofcom.org.uk
