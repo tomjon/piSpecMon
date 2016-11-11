3756aa01edcbfac3dcdd84e326a77f9a33b6672b
Tom Winch <tomw@flax.co.uk>
2016-11-11 10:12:42 +0000

    Improvement for dev usage

--- install.sh
+++ install.sh
@@ -74,11 +74,13 @@ hash a2ensite 2>/dev/null && (
 )
 
 # build the pi_control binary
-PI_CONTROL_PATH=`vbl PI_CONTROL_PATH`
-gcc -o spectrum/bin/pi_control spectrum/pi_control.c && (
-  sudo cp spectrum/bin/pi_control $PI_CONTROL_PATH
-  sudo chown root: $PI_CONTROL_PATH
-  sudo chmod a+s $PI_CONTROL_PATH
+[[ `uname -m` == arm* ]] && (
+  PI_CONTROL_PATH=`vbl PI_CONTROL_PATH`
+  gcc -o spectrum/bin/pi_control spectrum/pi_control.c && (
+    sudo cp spectrum/bin/pi_control $PI_CONTROL_PATH
+    sudo chown root: $PI_CONTROL_PATH
+    sudo chmod a+s $PI_CONTROL_PATH
+  )
 )
 
 # remind about post install steps
