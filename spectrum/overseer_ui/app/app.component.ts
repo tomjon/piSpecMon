d209999f8dbe16f8e27a48f7c160012511836c77
Tom Winch <tomw@flax.co.uk>
2016-12-01 11:56:01 +0000

    Under development

--- spectrum/overseer_ui/app/app.component.ts
+++ spectrum/overseer_ui/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
-import { DataService } from './data.service';
+import { Component } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { DataService } from './data.service';
+import { PsmComponent } from './psm.component';
+import { EventComponent } from './event.component';
 
 // Add the RxJS Observable operators we need in this app
 import './rxjs-operators';
@@ -9,19 +11,22 @@ export var TICK_INTERVAL = 2000;
 
 @Component({
   selector: 'overseer-app',
-  template: `<h1>PSM Overseer<h1>
+  template: `<h1>PSM Overseer</h1>
              <table>
                <tr *ngFor="let psm of data">
-                 <td>{{psm.name}}</td>
-                 <td>{{psm.heartbeat}}</td>
+                 <td><overseer-psm psm="psm"></overseer-psm></td>
+                 <td>
+                   <ol *ngFor="let event of psm.events">
+                     <li><overseer-event event="event"></overseer-event></li>
+                   </ol>
+                 </td>
                </tr>
              </table>`,
-  directives: [],
-  providers: [DataService, HTTP_PROVIDERS],
-  pipes: []
+  directives: [PsmComponent, EventComponent],
+  providers: [DataService, HTTP_PROVIDERS]
 })
 export class AppComponent {
-  data: any;
+  data: any = [];
 
   constructor(private dataService: DataService) { }
 
